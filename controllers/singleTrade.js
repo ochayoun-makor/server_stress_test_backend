cff345fd268e04d2030f6dc5820c12625f6f857f
nlister-makor <nlister@makor-capital.com>
2022-02-17 16:37:34 +0200

    connect with WS and Client

--- controllers/singleTrade.js
+++ controllers/singleTrade.js
@@ -1,11 +1,12 @@
 
+const { time } = require('console');
 const { promisify } = require('util');
 const exec = promisify(require('child_process').exec);
 
 module.exports.calcTradeTime = async function calcTradeTime(token, type, side, product_id, quantity) {
   // Exec output contains both stderr and stdout outputs
-  const timeOut = await exec(`Bash singleTradeScript.sh  ${token} ${type} ${side} ${product_id} ${quantity}`);
+  const timeOut = await exec(`Bash controllers/singleTradeScript.sh ${token} ${type} ${side} ${product_id} ${quantity}`);
   // const emailOutput = await exec('git config --global user.email');
-
+  
   return timeOut.stdout.trim();
 };
--- controllers/tradesController.js
+++ controllers/tradesController.js
@@ -1,5 +1,5 @@
-const axios = require('axios')
-const { calcTradeTime } = require('./singleTrade')
+const axios = require('axios');
+const { calcTradeTime } = require('./singleTrade');
 
 const tradesController = async (ws, reqData) => {
   // let types = 'MKT'
@@ -10,17 +10,17 @@ const tradesController = async (ws, reqData) => {
     const login_data = {
       username: 'client_test_2',
       password: 'Test123!',
-    }
+    };
     // Check the time and insert it to the object
-    const res = await axios.put('https://sb20.rest-api.enigma-securities.io/auth', login_data)
+    const res = await axios.put('https://sb20.rest-api.enigma-securities.io/auth', login_data);
     // console.log(tokenTime)
     // console.log(res.data, 'RES DATA')
-    const { key: token } = res.data
+    const { key: token } = res.data;
 
     if (token) {
-      axios.defaults.headers.common['Authorization'] = 'Bearer ' + token
+      axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
     } else {
-      delete axios.defaults.headers.common['Authorization']
+      delete axios.defaults.headers.common['Authorization'];
     }
 
     // check if data ...
@@ -29,37 +29,45 @@ const tradesController = async (ws, reqData) => {
     //   sides = reqData.filters.side
     // }
 
-    const types = reqData?.filters.type ? reqData.filters.type : 'MKT'
-    const sides = reqData?.filters.side ? reqData.filters.side : 'BUY'
+    const types = reqData?.filters.type ? reqData.filters.type : 'MKT';
+    const sides = reqData?.filters.side ? reqData.filters.side : 'BUY';
     // console.log(types, 'TUPES', sides, 'SIDES')
 
     if (reqData?.filters.product_id) {
       // bash single proudct
     } else {
       try {
-        const productsRes = await axios.get('https://sb20.rest-api.enigma-securities.io/product')
-
-        const { data: products } = productsRes
-        for (const product of products.slice(1,3)) {
-          const tradeTime = await calcTradeTime(token, types, sides, product.product_id, product.min_quantity)
-          console.log(tradeTime ,"TRADE TIME")
+        const productsRes = await axios.get('https://sb20.rest-api.enigma-securities.io/product');
+        const { data: products } = productsRes;
+        for (const product of products.slice(1, 3)) {
+          const { product_id, min_quantity } = product;
+          const tradeTime = await calcTradeTime(token, types, sides, product_id, min_quantity);
+          const data_to_return = {
+            type: 'trade',
+            data: {
+              type: types,
+              side: sides,
+              product_id,
+              quantity: min_quantity,
+              tradeTime,
+            },
+          };
+          ws.send(JSON.stringify(data_to_return));
         }
-  
 
         // } else {
         //   const types = ['MKT']
         //   const sides = ['BUY']
         // }
       } catch (error) {
-        console.log(error.message)
-        throw new Error('data not found')
+        console.log(error.message);
+        throw new Error('data not found');
       }
     }
   } catch (error) {
     // ws.send({})
   }
-}
-tradesController('test')
+};
 module.exports = {
   tradesController,
-}
+};
